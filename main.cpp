1067089ebabff8324c0ea6b4f48a34c4a801c169
GiovanniBestoso <115479602+GiovanniBestoso@users.noreply.github.com>
2022-11-17 16:25:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+    float a=0, b=0, x=0;
+    int opzione=0;
+    cin>>a>>b>>opzione;
+    switch (opzione){
+        case 0:
+            x=(a*b)/2;
+            cout<< x<<endl;
+            break;
+        case 1:
+            x=a*a;
+            cout<<x<<endl;
+            break;
+        case 2:
+            x=a*b;
+            cout<<x<<endl;
+        default:
+            cout<<"opzione non valida"<<endl;
+    }
    return 0;
 }
